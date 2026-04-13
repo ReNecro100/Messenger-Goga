35bb18f61799acc985ff714e9668a434b59bad3c
ReNecro100 <jarabotaju2007@gmail.com>
2026-04-13 19:22:01 +0300

    required functions are done!!!

--- MessengerGoga/talking/views.py
+++ MessengerGoga/talking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import User, ChatMessage, Chat
+from django.db.models import Count, Subquery, OuterRef
 from .forms import UserFormReg, UserFormEdit, UserFormLogin, ChatFormCreate, ChatFormEdit
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
@@ -16,13 +17,45 @@ def wsschat(request, room_name):
     if request.method == 'POST' and 'non_member' in request.POST:
         chat = Chat.objects.get(id=int(room_name))
         chat.members.add(User.objects.get(id=request.POST['non_member']))
+    if request.method == 'POST' and 'member' in request.POST:
+        chats = Chat.objects.filter(
+            chat_type=2,
+            members__id__in=[request.POST['member'], request.user.id]
+        ).annotate(
+            member_count=Count('members')
+        ).filter(
+            member_count=2
+        )
+        try:
+            return redirect(f'/wsschat/{chats[0].id}')
+        except:
+            chat_form = ChatFormCreate(data={
+                "name": f"{request.user.username} - {User.objects.get(id=request.POST['member'])}",
+                "description": f"ЛС пользователей {request.user.username} и {User.objects.get(id=request.POST['member'])}",
+                "chat_type": 2
+                })
+            print(chat_form.is_valid())
+            if chat_form.is_valid():
+                lechat = chat_form.save(request, commit=False)
+                lechat.save()
+                lechat.members.add(User.objects.get(id=request.POST['member']))
+                lechat.members.add(request.user)
+                return redirect(f'/wsschat/{lechat.id}')
+        
+
     return render(request, "wsschat.html", {
         "room_name": room_name, 
         'current_user': request.user, 
         'current_chat': Chat.objects.get(id=int(room_name)),
         'my_chats': request.user.member_of.all(),
         'members_count': Chat.objects.get(id=int(room_name)).members.count(),
-        'non_members': User.objects.exclude(member_of=room_name) #select * from members where chat_id!={value}
+        'members': User.objects.filter(member_of=room_name).exclude(id=request.user.id),
+        'non_members': User.objects.filter(
+                            member_of__chat_type_id=2,           # чаты типа 2
+                            member_of__members__id=14            # в чате есть пользователь 14
+                        ).exclude(
+                            id=14
+                        ).distinct() #User.objects.exclude(member_of=room_name)
     })
 
 @login_required(redirect_field_name="log", login_url='log')
@@ -43,9 +76,9 @@ def new_chat(request):
 
 def root_redirect(request):
     if request.user.is_authenticated:
-        return redirect('wsschat/2') #Pomeniatj idshnik pri zapuske v obraschenije <-VAZHNO SHO KAPEC
+        return redirect('/wsschat/2') #Pomeniatj idshnik pri zapuske v obraschenije <-VAZHNO SHO KAPEC
     else:
-        return redirect('log')
+        return redirect('/log')
 
 def secret(request):
     return HttpResponse("Как ты сюда попал???")
